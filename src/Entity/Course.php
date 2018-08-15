714d8b4b65ef3f878a52516676f069dd365f6222
Samuel Solis <ssolisfu@gmail.com>
2018-08-15 10:30:52 +0200

    Added Course Entity.

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -1,5 +1,24 @@
 <?php
 
-/*
- * @file Course Entity Class should be here.
+namespace Drupal\course\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+
+
+/**
+ * Defines the Course entity.
+ *
+ * @ingroup course
+ *
+ * @ContentEntityType(
+ *   id = "course",
+ *   label = @Translation("Course entity"),
+ *   base_table = "courses",
+ *   entity_keys = {},
+ *   admin_permission = "administer courses",
+ *   handlers = {},
+ *   },
+ *   links = {},
+ * )
  */
+class Course extends ContentEntityBase {}
