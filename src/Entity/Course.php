6a42a5dcd53527a0bac5555281988e5e5b8b2f3a
Samuel Solis <ssolisfu@gmail.com>
2018-08-15 11:20:20 +0200

    Add some fields to the course Entity

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -3,6 +3,8 @@
 namespace Drupal\course\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 
 
 /**
@@ -14,11 +16,47 @@ use Drupal\Core\Entity\ContentEntityBase;
  *   id = "course",
  *   label = @Translation("Course entity"),
  *   base_table = "courses",
- *   entity_keys = {},
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "title",
+ *   },
  *   admin_permission = "administer courses",
  *   handlers = {},
- *   },
  *   links = {},
  * )
  */
-class Course extends ContentEntityBase {}
+class Course extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Define the field properties here.
+   *
+   * Field name, type and size determine the table structure.
+   *
+   * In addition, we can define how the field and its content can be manipulated
+   * in the GUI. The behaviour of the widgets used can be determined here.
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Course id'))
+      ->setDescription(t('Course id to identify the course.'))
+      ->setSettings([
+        'default_value' => NULL,
+      ])
+      ->setSetting('unsigned', TRUE)
+      ->setReadOnly(TRUE);
+
+    $fields['title'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Title'))
+      ->setDescription(t('Course title.'))
+      ->setSettings([
+        'default_value' => '',
+        'max_length' => 255,
+        'text_processing' => 0,
+      ]);
+
+    return $fields;
+  }
+}
